732b5a9fa5bdc98e0c971020bbe4c80b0fe8b446
mahen <mahendrasridayarathna@gmail.com>
2021-05-18 21:59:39 +0530

    Add sub url logic and db script

---
+++ crawler_db.sql
@@ -0,0 +1,39 @@
+/*
+SQLyog Ultimate v8.55 
+MySQL - 5.6.51-log : Database - crawler_db
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`crawler_db` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `crawler_db`;
+
+/*Table structure for table `locations` */
+
+DROP TABLE IF EXISTS `locations`;
+
+CREATE TABLE `locations` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `parent_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_locations` (`parent_id`),
+  CONSTRAINT `FK_locations` FOREIGN KEY (`parent_id`) REFERENCES `locations` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `locations` */
+
+insert  into `locations`(`id`,`name`,`parent_id`) values (1,'Sri Lanka',NULL),(2,'Colombo',1),(3,'United Kingdom',NULL),(4,'London',3),(5,'Trinco',1),(6,'Arugambay',1),(7,'Sigiriya',1),(8,'Galle',1);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
--- routes/index.js
+++ routes/index.js
@@ -5,88 +5,106 @@ const request = require('request');
 const cheerio = require('cheerio');
 const fs = require('fs');
 const axios = require('axios');
+const validUrl = require('valid-url');
 
 
 const urls = "https://www.srilanka.travel/,http://www.tourismmin.gov.lk/";
 var countrySet = new Set();
 var cityMap = [];
 var cityCountryMap = new Map();
+var array = fs.readFileSync('Keywords.txt').toString().split("\n");
+const checkedUrl = {};
 
-/* GET index page. */
-router.get('/', function (req, res, next) {
+/**
+ * Get Valid Url
+ * @param {*} link 
+ * @returns 
+ */
+const getValidUrl = (link) => {
+  if (validUrl.isUri(link)) {
+    return link;
+  } else {
+    return "https://www.srilanka.travel/";
+  }
+}
 
-  // Load Location by database
-  loadLocations();
+/**
+ * Crawling Web Site By URL
+ * @param {*} url 
+ * @returns 
+ */
+async function crawlerWebSite(url) {
+  if (checkedUrl[url]) return;
+  checkedUrl[url] = true;
+  await fetchData(url).then((res) => {
+    const html = res.data;
+    const $ = cheerio.load(html);
+    const htmlTag = $('h1,h2,h3,h4,h5,h6,p,body');
 
-  var array = fs.readFileSync('Keywords.txt').toString().split("\n");
-  var urlArray = urls.split(",");
-
-  // Iterate Url and Fetch web site data.
-  urlArray.forEach(url => {
-    fetchData(url).then((res) => {
-      const html = res.data;
-      const $ = cheerio.load(html);
-      const statsTable = $('h1,h2,h3,h4,h5,h6,p,body');
-
-      var matchedKeyWords = new Set();
-      var matchedCountry = new Set();
-      var matchedCity = new Set();
-      var matchedLocations = new Set();
-
-      statsTable.each(function () {
-        let title = $(this).text();
-        array.forEach(keyword => {
-          if (title.includes(keyword.trim())) {
-            matchedKeyWords.add(keyword.trim());
-          }
-        });
-
-        // Find Matched Country
-        countrySet.forEach(country => {
-          if (title.includes(country)) {
-            console.log("country" + country);
-            matchedCountry.add(country);
-          }
-        });
-
-        // Find Matched City
-        cityMap.forEach(city => {
-          if (title.includes(city)) {
-            console.log("city" + city);
-            matchedCity.add(city);
-          }
-        });
-
-        // Add Unique Locations
-        var locations = [];
-        matchedCountry.forEach(c => {
-          locations.push(c);
-          matchedCity.forEach(city => {
-            if (cityCountryMap.get(city) === c) {
-              locations.push(city);
-            }
-          })
-        });
-        matchedLocations.add(locations.toString());
-
-      });
-
-      // Write To File 
-      var file = `============================== \nURL: ${url}\nKeywords found: ${Array.from(matchedKeyWords).toString()}\nLocation mentioned: ${Array.from(matchedLocations).toString()}\n\n`;
-      fs.appendFile("Response.txt", file, (error) => {
-        if (!error) {
-          console.log("File Write");
-        } else {
-          console.log(error);
+    var matchedKeyWords = new Set();
+    var matchedCountry = new Set();
+    var matchedCity = new Set();
+    var matchedLocations = new Set();
+    
+    htmlTag.each(function () {
+      let title = $(this).text();
+      array.forEach(keyword => {
+        if (title.includes(keyword.trim())) {
+          matchedKeyWords.add(keyword.trim());
         }
       });
+
+      // Find Matched Country
+      countrySet.forEach(country => {
+        if (title.includes(country)) {
+          console.log("country" + country);
+          matchedCountry.add(country);
+        }
+      });
+
+      // Find Matched City
+      cityMap.forEach(city => {
+        if (title.includes(city)) {
+          console.log("city" + city);
+          matchedCity.add(city);
+        }
+      });
+
+      // Add Unique Locations
+      var locations = [];
+      matchedCountry.forEach(c => {
+        locations.push(c);
+        matchedCity.forEach(city => {
+          if (cityCountryMap.get(city) === c) {
+            locations.push(city);
+          }
+        })
+      });
+      matchedLocations.add(locations.toString());
+
+    });
+    // Write To File 
+    var file = `============================== \nURL: ${url}\nKeywords found: ${Array.from(matchedKeyWords).toString()}\nLocation mentioned: ${Array.from(matchedLocations).toString()}\n\n`;
+    fs.appendFile("Response.txt", file, (error) => {
+      if (!error) {
+        console.log("File Write");
+      } else {
+        console.log(error);
+      }
+    });
+
+    let urlCount = 0;
+    const $a = cheerio.load(html);
+    const linkTag = $a('a').map((i, link) => link.attribs.href).get();
+    linkTag.forEach(link => {
+      if (urlCount === 5) return;
+      console.log(getValidUrl(link));
+      crawlerWebSite(getValidUrl(link));
+      urlCount++;
     });
 
   });
-  res.send('File Write Succesfully');
-});
-
-
+}
 /**
  * Load web site 
  * @param {*} url 
@@ -110,9 +128,9 @@ async function loadLocations() {
   await mysqlCoonection.query("SELECT l2.name AS city , l1.name AS country FROM locations l1 INNER JOIN locations l2  ON l1.id = l2.parent_id", (error, rows, fields) => {
     if (!error) {
       rows.forEach(line => {
-        console.log("Country" + line.country);
+        console.log("Country : " + line.country);
         countrySet.add(line.country);
-        console.log("City" + line.country);
+        console.log("City : " + line.country);
         cityMap.push(line.city);
         cityCountryMap.set(line.city, line.country)
       })
@@ -121,4 +139,18 @@ async function loadLocations() {
     }
   })
 }
+
+/* GET index page. */
+router.get('/', function (req, res, next) {
+  // Load Location by database
+  loadLocations();
+  var urlArray = urls.split(",");
+
+  // Iterate Url and Fetch web site data.
+  urlArray.forEach(url => {
+    crawlerWebSite(url);
+  });
+
+  res.send('File Write Succesfully');
+});
 module.exports = router;
